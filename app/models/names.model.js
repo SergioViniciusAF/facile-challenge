67b51441f32f188e160e9617d379ea85f8796c00
SergioViniciusAF <79481502+SergioViniciusAF@users.noreply.github.com>
2021-12-01 19:19:32 -0300

    Criação do names.model.js
    Criação do names.model.js

---
+++ app/models/names.model.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize/dist");
+
+module.exports = (sequelize, Sequelize) => {
+    const Names = sequelize.define("names", {
+        id:{
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        encripted_name: {
+            type: Sequelize.STRING
+        }
+
+    });
+    return Names;
+};
